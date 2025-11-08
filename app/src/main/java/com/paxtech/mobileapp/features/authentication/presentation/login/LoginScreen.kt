f2441b0f0a48e9c183f6070c9241d68306ada03d
gael-rs <gaelrszk@gmail.com>
2025-11-08 11:21:51 -0500

    fix: import new tools

--- app/src/main/java/com/paxtech/mobileapp/features/authentication/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/paxtech/mobileapp/features/authentication/presentation/login/LoginScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
